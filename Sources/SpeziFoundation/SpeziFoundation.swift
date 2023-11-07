26847c813cb2323015911c73d0c222dcab5f88e1
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-11-06 16:12:16 -0800

    Initial Setup (#1)
    # Initial Setup
    
    
    ## :gear: Release Notes 
    - Initial Setup
    - CI Improvement
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ Sources/SpeziFoundation/SpeziFoundation.swift
@@ -0,0 +1,19 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+/// Spezi Foundation provides a base layer of functionality useful in many applications, including fundamental types, algorithms, extensions, and data structures.
+public struct SpeziFoundation {
+    /// Stanford University.
+    public var stanford: String {
+        "Stanford University"
+    }
+    
+    
+    /// Documentation ...
+    public init() {}
+}
