7bf97d5abf7b1adc806c0c168ba619d80e2a2b29
Lukas Kollmer <hey@lukaskollmer.de>
2025-04-11 12:52:58 +0200

    add RuntimeEnvironment.swift

---
+++ Sources/SpeziFoundation/Misc/RuntimeEnvironment.swift
@@ -0,0 +1,56 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import Foundation
+#if os(macOS) || targetEnvironment(macCatalyst)
+import Security
+#endif
+
+
+extension ProcessInfo {
+    /// Whether the application is currently running in a sandbox.
+    ///
+    /// This value will always be `true` when running on iOS, watchOS, visionOS, or tvOS.
+    public static let isRunningInSandbox: Bool = {
+    #if !(os(macOS) || targetEnvironment(macCatalyst))
+        // If we're not running on macOS or macCatalyst, we're running on iOS/watchOS/visionOS/tvOS which always have a sandbox
+        return true
+    #else
+        var `self`: SecCode?
+        guard SecCodeCopySelf([], &self) == errSecSuccess else {
+            return false
+        }
+        var codeSignInfo: CFDictionary?
+        guard SecCodeCopySigningInformation(
+            unsafeBitCast(self, to: SecStaticCode.self),
+            SecCSFlags(rawValue: kSecCSDynamicInformation),
+            &codeSignInfo
+        ) == errSecSuccess else {
+            return false
+        }
+        guard let codeSignInfo = codeSignInfo.map({ $0 as NSDictionary }),
+              let entitlementsDict = codeSignInfo[kSecCodeInfoEntitlementsDict] as? NSDictionary else {
+            return false
+        }
+        if let sandboxEntry = entitlementsDict["com.apple.security.app-sandbox"] as? NSNumber {
+            return sandboxEntry.boolValue
+        } else {
+            return false
+        }
+    #endif
+    }()
+    
+    
+    /// Whether the application is currently being run as part of a XCTest.
+    ///
+    /// - Note: This value does **not** indicate whether the application is currently being tested; for example, it will be `false` for an app currently being UI-tested,
+    /// since in that case the app itself will be running in a separate process from the actual test target (for which this value would be `true`).
+    public static var isRunningInXCTest: Bool {
+        NSClassFromString("XCTestCase") != nil
+    }
+}
