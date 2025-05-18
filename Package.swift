b85375edb66e3125e21c980b1cfa0a137a0e50ec
Lukas Kollmer <hey@lukaskollmer.de>
2025-05-18 13:45:32 +0200

    update XCTRuntimeAssertions dep

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
-        .package(url: "https://github.com/StanfordBDHG/XCTRuntimeAssertions.git", from: "1.2.0")
+        .package(url: "https://github.com/StanfordBDHG/XCTRuntimeAssertions.git", branch: "lukas/fix")
     ] + swiftLintPackage(),
     targets: [
         .target(
