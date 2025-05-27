404dd76502fed1e0cc03dcdb8bba9818f206b244
Andreas Bauer <andreas.bauer@stanford.edu>
2025-05-28 01:17:40 +0200

    Support cancelable Child Tasks (#30)
    # Support cancelable Child Tasks
    
    ## :recycle: Current situation & Problem
    In certain cases when working with task groups, it is helpful to have
    control over child task cancellation. This PR adds a helper method to
    easily construct such cancelable child tasks.
    
    
    ## :gear: Release Notes
    * Add `DiscardingTaskGroup/addCancelableTask(_:)`
    
    
    ## :books: Documentation
    Added to DocC catalog.
    
    
    ## :white_check_mark: Testing
    Two new unit tests that confirm the not cancelled and cancelled case.
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ Tests/SpeziFoundationTests/CancelableChildTaskTests.swift
@@ -0,0 +1,49 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SpeziFoundation
+import Testing
+
+@Suite("Cancelable Child Task")
+struct CancelableChildTaskTests {
+    @Test("Normal Completion")
+    func testNormalCompletion() async {
+        await withDiscardingTaskGroup { group in
+            await confirmation { confirmation in
+                let handle = group.addCancelableTask {
+                    try? await Task.sleep(for: .milliseconds(10), tolerance: .nanoseconds(0))
+                    confirmation()
+                }
+
+                try? await Task.sleep(for: .milliseconds(100), tolerance: .nanoseconds(0))
+                handle.cancel()
+            }
+        }
+    }
+
+    @Test("Cancelation")
+    func testCancelation() async {
+        await withDiscardingTaskGroup { group in
+            await confirmation { confirmation in
+                let handle = group.addCancelableTask {
+                    do {
+                        try await Task.sleep(for: .milliseconds(30), tolerance: .nanoseconds(0))
+                        Issue.record("Task was not cancelled!")
+                    } catch {
+                        confirmation()
+                    }
+                }
+
+                try? await Task.sleep(for: .milliseconds(5), tolerance: .nanoseconds(0))
+                handle.cancel()
+
+                try? await Task.sleep(for: .milliseconds(50), tolerance: .nanoseconds(0))
+            }
+        }
+    }
+}
