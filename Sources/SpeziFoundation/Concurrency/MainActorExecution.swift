c77c2b7637fc153b12e1815514a50802b671d378
Andreas Bauer <andreas.bauer@stanford.edu>
2025-06-11 22:53:01 +0200

    Add docs

--- Sources/SpeziFoundation/Concurrency/MainActorExecution.swift
+++ Sources/SpeziFoundation/Concurrency/MainActorExecution.swift
@@ -10,8 +10,12 @@ import Foundation
 
 
 /// Runs or schedules a closure on the `MainActor`, depending on the current execution context.
+///
 /// If the function is already running on the `MainActor`, the closure will be invoked immediately.
 /// Otherwise, an invocation of the closure will be scheduled onto the `MainActor`.
+///
+/// - Important: Do not use this method as it breaks structured concurrency. Instead you might choose to use a combination of the `task(_:)` modifier and an `AsyncStream` to schedule work
+///     onto the MainActor. If working within Spezi Modules, explore the `ServiceModule` protocol to add structured concurrency support to your Module.
 @available(*, deprecated, message: "Please do not use this method as it breaks structured concurrency. Consider using other mechanisms like .task(_:).")
 public func runOrScheduleOnMainActor(
     _ block: @MainActor @escaping () -> Void
