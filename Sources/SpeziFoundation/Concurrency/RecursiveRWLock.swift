df38d0119950dce16fe189762cba130a573eb59e
Lukas Kollmer <hey@lukaskollmer.de>
2025-08-27 14:51:03 +0200

    make the primary {Recursive}RWLock API inlinable

--- Sources/SpeziFoundation/Concurrency/RecursiveRWLock.swift
+++ Sources/SpeziFoundation/Concurrency/RecursiveRWLock.swift
@@ -14,8 +14,8 @@ import Foundation
 /// Recursive Read-Write Lock using `pthread_rwlock`.
 ///
 /// This is a recursive version of the ``RWLock`` implementation.
-public final class RecursiveRWLock: PThreadReadWriteLock, @unchecked Sendable {
-    let rwLock: UnsafeMutablePointer<pthread_rwlock_t>
+public final class RecursiveRWLock: _PThreadReadWriteLock, @unchecked Sendable {
+    public let _rwLock: UnsafeMutablePointer<pthread_rwlock_t> // swiftlint:disable:this identifier_name
 
     private let writerThread = ManagedAtomic<pthread_t?>(nil)
     private var writerCount = 0
@@ -23,44 +23,33 @@ public final class RecursiveRWLock: PThreadReadWriteLock, @unchecked Sendable {
 
     /// Create a new recursive read-write lock.
     public init() {
-        rwLock = Self.pthreadInit()
+        _rwLock = Self.pthreadInit()
     }
-
-
-    private func writeLock() {
-        let selfThread = pthread_self()
-
-        if let writer = writerThread.load(ordering: .relaxed),
-           pthread_equal(writer, selfThread) != 0 {
-            // we know that the writerThread is us, so access to `writerCount` is synchronized (its us that holds the rwLock).
-            writerCount += 1
-            assert(writerCount > 1, "Synchronization issue. Writer count is unexpectedly low: \(writerCount)")
-            return
+    
+    
+    // MARK: Operations
+    
+    @inlinable
+    public func withReadLock<Result, E>(_ body: () throws(E) -> Result) throws(E) -> Result {
+        _readLock()
+        defer {
+            _readUnlock()
         }
-
-        pthreadWriteLock()
-
-        writerThread.store(selfThread, ordering: .relaxed)
-        writerCount = 1
+        return try body()
     }
 
-    private func writeUnlock() {
-        // we assume this is called while holding the write lock, so access to `writerCount` is safe
-        if writerCount > 1 {
-            writerCount -= 1
-            return
+    @inlinable
+    public func withWriteLock<Result, E>(_ body: () throws(E) -> Result) throws(E) -> Result {
+        _writeLock()
+        defer {
+            _writeUnlock()
         }
-
-        // otherwise it is the last unlock
-        writerThread.store(nil, ordering: .relaxed)
-        writerCount = 0
-
-        pthreadUnlock()
+        return try body()
     }
-
-    private func readLock() {
+    
+    @usableFromInline
+    func _readLock() { // swiftlint:disable:this identifier_name
         let selfThread = pthread_self()
-
         if let writer = writerThread.load(ordering: .relaxed),
            pthread_equal(writer, selfThread) != 0 {
             // we know that the writerThread is us, so access to `readerCount` is synchronized (its us that holds the rwLock).
@@ -68,44 +57,50 @@ public final class RecursiveRWLock: PThreadReadWriteLock, @unchecked Sendable {
             assert(readerCount > 0, "Synchronization issue. Reader count is unexpectedly low: \(readerCount)")
             return
         }
-
-        pthreadReadLock()
+        _pthreadReadLock()
     }
 
-    private func readUnlock() {
+    
+    // MARK: Implementation
+    
+    @usableFromInline
+    func _readUnlock() { // swiftlint:disable:this identifier_name
         // we assume this is called while holding the reader lock, so access to `readerCount` is safe
         if readerCount > 0 {
             // fine to go down to zero (we still hold the lock in write mode)
             readerCount -= 1
             return
         }
+        _pthreadUnlock()
+    }
+    
 
-        pthreadUnlock()
+    @usableFromInline
+    func _writeLock() { // swiftlint:disable:this identifier_name
+        let selfThread = pthread_self()
+        if let writer = writerThread.load(ordering: .relaxed),
+           pthread_equal(writer, selfThread) != 0 {
+            // we know that the writerThread is us, so access to `writerCount` is synchronized (its us that holds the rwLock).
+            writerCount += 1
+            assert(writerCount > 1, "Synchronization issue. Writer count is unexpectedly low: \(writerCount)")
+            return
+        }
+        _pthreadWriteLock()
+        writerThread.store(selfThread, ordering: .relaxed)
+        writerCount = 1
     }
 
-
-    /// Call `body` with a writing lock.
-    ///
-    /// - Parameter body: A function that writes a value while locked, then returns some value.
-    /// - Returns: The value returned from the given function.
-    public func withWriteLock<T>(body: () throws -> T) rethrows -> T {
-        writeLock()
-        defer {
-            writeUnlock()
+    @usableFromInline
+    func _writeUnlock() { // swiftlint:disable:this identifier_name
+        // we assume this is called while holding the write lock, so access to `writerCount` is safe
+        if writerCount > 1 {
+            writerCount -= 1
+            return
         }
-        return try body()
-    }
-
-    /// Call `body` with a reading lock.
-    ///
-    /// - Parameter body: A function that reads a value while locked.
-    /// - Returns: The value returned from the given function.
-    public func withReadLock<T>(body: () throws -> T) rethrows -> T {
-        readLock()
-        defer {
-            readUnlock()
-        }
-        return try body()
+        // otherwise it is the last unlock
+        writerThread.store(nil, ordering: .relaxed)
+        writerCount = 0
+        _pthreadUnlock()
     }
 
     deinit {
