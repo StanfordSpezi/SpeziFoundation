31c4a0c1bfd11124ed1c8a5ab59685dfbbaed7bd
Philipp Nagy <philipp.nagy@tum.de>
2025-10-22 14:44:38 +0200

    Add timing logs to cancelation test in CancelableChildTaskTests

--- Tests/SpeziFoundationTests/CancelableChildTaskTests.swift
+++ Tests/SpeziFoundationTests/CancelableChildTaskTests.swift
@@ -8,6 +8,7 @@
 
 import SpeziFoundation
 import Testing
+import Foundation
 
 
 struct CancelableChildTaskTests {
@@ -28,17 +29,22 @@ struct CancelableChildTaskTests {
     @Test
     func cancelation() async {
         await withDiscardingTaskGroup { group in
+            let start = Date()
             await confirmation { confirmation in
                 let handle = group.addCancelableTask {
                     do {
                         try await Task.sleep(for: .milliseconds(60), tolerance: .nanoseconds(0))
                         Issue.record("Task was not cancelled!")
+                        let duration = -start.timeIntervalSinceNow 
+                        print("Task ran for \(duration) seconds")
                     } catch {
                         confirmation()
                     }
                 }
                 try? await Task.sleep(for: .milliseconds(10), tolerance: .nanoseconds(0))
                 handle.cancel()
+                let duration = -start.timeIntervalSinceNow
+                print("Cancel was called after \(duration) seconds")
                 try? await Task.sleep(for: .milliseconds(100), tolerance: .nanoseconds(0))
             }
         }
