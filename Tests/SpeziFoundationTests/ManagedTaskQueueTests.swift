StanfordSpezi/SpeziFoundation

commit 111e480c964723146673e85e02e825dc358910bf

add `ManagedTaskQueue` (#50)
# add `ManagedTaskQueue`

## :recycle: Current situation & Problem
adds a `withManagedTaskQueue(limit:_:)` API, which wraps a `TaskGroup`
and enforces a concurrency limit, i.e. a limit how many of the child
tasks can run at the same time.


## :gear: Release Notes
- added `withManagedTaskQueue(limit:_:)`


## :books: Documentation
yes


## :white_check_mark: Testing
yes


## :pencil: Code of Conduct & Contributing Guidelines
By creating and submitting this pull request, you agree to follow our
[Code of
Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
and [Contributing
Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
- [x] I agree to follow the [Code of
Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
and [Contributing
Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

Sources/SpeziFoundation/Concurrency/ManagedTaskQueue.swift
@@ -1,0 +1,99 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+
+/// A Managed Task Queue
+///
+/// Your code does not use this type directly; instead it is used by ``withManagedTaskQueue(limit:_:)`` and allows you to add child tasks.
+public struct ManagedTaskQueue: ~Copyable {
+    @usableFromInline typealias Operation = @Sendable () async -> Void
+    
+    @usableFromInline let continuation: AsyncStream<Operation>.Continuation
+    
+    fileprivate init(continuation: AsyncStream<Operation>.Continuation) {
+        self.continuation = continuation
+    }
+    
+    /// Submits an operation to be executed by the Managed Task Queue.
+    @inlinable
+    public func addTask(_ operation: @escaping @Sendable () async -> Void) {
+        continuation.yield(operation)
+    }
+}
+
+
+/// Runs a dynamic number of child tasks, with a maximum concurrency limit.
+///
+/// The function will return once all child tasks scheduled via ``ManagedTaskQueue/addTask(_:)`` have completed.
+///
+/// ```swift
+/// // Fetches and uploads data, in a way that at most 4 sensors are processed at a time.
+/// func uploadNewData(for sensors: [Sensor]) async {
+///     await withManagedTaskQueue(limit: 4) { taskQueue in
+///         for sensor in sensors {
+///             taskQueue.addTask {
+///                 let data = await sensor.fetch()
+///                 await upload(data)
+///             }
+///         }
+///     }
+/// }
+/// ```
+///
+/// - parameter limit: The maximum number of child tasks that are allowed to run at the same time.
+/// - parameter body: Will be called with a ``ManagedTaskQueue`` that is used for registering tasks.
+///
+/// ## Topics
+/// ### Classes
+/// - ``ManagedTaskQueue``
+public func withManagedTaskQueue(limit: Int, _ body: sending @escaping (_ taskQueue: borrowing ManagedTaskQueue) async -> Void) async {
+    /// Used to differentiate between the 2 kinds of tasks we schedule with the underlying `TaskGroup`:
+    /// - the initial "scheduler" task, which calls `body` to schedule the individual operations with the ``ManagedTaskQueue``
+    /// - the worker tasks, which run the operations scheduled on the ``ManagedTaskQueue``.
+    enum TaskType {
+        case scheduler
+        case worker
+    }
+    let (stream, continuation) = AsyncStream.makeStream(of: ManagedTaskQueue.Operation.self)
+    // we need to wrap this as a workaround to be able to mark the `body` parameter as `sending` instead of having to make it `@Sendable`
+    let boxedBody = { body }
+    await withTaskGroup(of: TaskType.self) { group in
+        let body = (consume boxedBody)()
+        group.addTask {
+            await body(ManagedTaskQueue(continuation: continuation))
+            continuation.finish()
+            return .scheduler
+        }
+        var activeWorkers = 0 {
+            didSet { assert(activeWorkers <= limit) }
+        }
+        for await operation in stream {
+            if activeWorkers >= limit {
+                // we're at the limit, and need to wait for a slot to become available.
+                // we can't simply call `group.next()` and ignore the result,
+                // since the first task will be the initial one that calls `body`.
+                while let taskType = await group.next() {
+                    switch taskType {
+                    case .scheduler:
+                        continue
+                    case .worker:
+                        break
+                    }
+                }
+                activeWorkers -= 1
+            }
+            group.addTask {
+                await operation()
+                return .worker
+            }
+            activeWorkers += 1
+        }
+        // finish all worker tasks before returning from the function
+        await group.waitForAll()
+    }
+}

Tests/SpeziFoundationTests/ManagedTaskQueueTests.swift
@@ -1,0 +1,69 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+// swiftlint:disable force_try
+
+import Foundation
+import SpeziFoundation
+import Testing
+
+
+@Suite
+struct ManagedTaskQueueTests {
+    @MainActor
+    private final class OperationsTracker: Sendable {
+        let expectedLimit: Int
+        private(set) var active = Set<Int>()
+        private(set) var completed = Set<Int>()
+        private(set) var maxObservedConcurrency = 0
+        
+        nonisolated init(expectedLimit: Int) {
+            self.expectedLimit = expectedLimit
+        }
+        
+        func trackBegin(of operation: Int) throws {
+            #expect(
+                active.count < expectedLimit,
+                "adding \(operation); active: \(active.sorted()); completed: \(completed.sorted())"
+            )
+            try #require(!completed.contains(operation), "Operation '\(operation)' has already run")
+            try #require(active.insert(operation).inserted, "Operation '\(operation)' is already running")
+            maxObservedConcurrency = max(maxObservedConcurrency, active.count)
+        }
+        
+        func trackEnd(of operation: Int) throws {
+            #expect(
+                active.count <= expectedLimit,
+                "removing \(operation); active: \(active.sorted()); completed: \(completed.sorted())"
+            )
+            try #require(!completed.contains(operation), "Operation '\(operation)' has already run")
+            let idx = try #require(active.firstIndex(of: operation), "Operation '\(operation)' is not running")
+            active.remove(at: idx)
+            completed.insert(operation)
+        }
+    }
+    
+    
+    @Test(arguments: Array(1...17))
+    func managedTaskQueue(concurrencyLimit: Int) async {
+        let numTasks = concurrencyLimit * 3
+        let tracker = OperationsTracker(expectedLimit: concurrencyLimit)
+        await withManagedTaskQueue(limit: concurrencyLimit) { taskQueue in
+            for idx in 0..<numTasks {
+                taskQueue.addTask {
+                    try! await tracker.trackBegin(of: idx)
+                    try! await Task.sleep(for: .seconds(0.25))
+                    try! await tracker.trackEnd(of: idx)
+                }
+            }
+        }
+        #expect(await tracker.active.isEmpty)
+        #expect(await tracker.completed == Set(0..<numTasks))
+        #expect(await tracker.maxObservedConcurrency <= concurrencyLimit)
+    }
+}

.github/workflows/build-and-test.yml
@@ -64,8 +64,8 @@ jobs:
       destination: ${{ matrix.platform.destination }}
       resultBundle: ${{ format('TestApp-{0}-{1}.xcresult', matrix.platform.name, matrix.config) }}
       artifactname: ${{ format('TestApp-{0}-{1}.xcresult', matrix.platform.name, matrix.config) }}
-  buildandtest_linux:
-    name: Build and Test Swift Package Linux
+  package_tests_linux:
+    name: Build and Test Swift Package Linux (${{ matrix.config }})
     uses: StanfordBDHG/.github/.github/workflows/swift-test.yml@v2
     strategy:
       matrix:
@@ -74,7 +74,7 @@ jobs:
       artifact_name: ${{ format('SpeziFoundation-Package-Linux-{0}.lcov', matrix.config) }}
   uploadcoveragereport:
     name: Upload Coverage Report
-    needs: [package_tests, ui_tests, buildandtest_linux]
+    needs: [package_tests, package_tests_linux, ui_tests]
     uses: StanfordBDHG/.github/.github/workflows/create-and-upload-coverage-report.yml@v2
     with:
       coveragereports: SpeziFoundation-Package-*.xcresult TestApp-*.xcresult

Sources/SpeziFoundation/Markdown Document/MarkdownDocument.swift
@@ -16,7 +16,7 @@ public import Foundation
 ///
 /// ### Metadata
 ///
-/// When using any of the `processing:` initializers to create a ``MarkdownDocument`` (e.g.: ``init(processing:customElementNames:)-(String,_)``),
+/// When using any of the `processing:` initializers to create a ``MarkdownDocument`` (e.g.: ``init(processing:customElementNames:baseUrl:)-(String,_,_)``),
 /// any frontmatter-style metadata entries at the very beginning of the input will be parsed into a ``Metadata-swift.struct`` object.
 ///
 /// The frontmatter-style metadata takes the following format:
@@ -93,10 +93,10 @@ public import Foundation
 /// ## Topics
 ///
 /// ### Initializers
-/// - ``init(metadata:blocks:)``
-/// - ``init(processing:customElementNames:)-(String,Set<String>)``
-/// - ``init(processing:customElementNames:)-(Data,Set<String>)``
-/// - ``init(processingContentsOf:customElementNames:)``
+/// - ``init(metadata:blocks:baseUrl:)``
+/// - ``init(processing:customElementNames:baseUrl:)-(String,_,_)``
+/// - ``init(processing:customElementNames:baseUrl:)-(Data,_,_)``
+/// - ``init(contentsOf:customElementNames:baseUrl:)``
 /// - ``ParseError``
 ///
 /// ### Instance Properties
@@ -178,6 +178,7 @@ public struct MarkdownDocument: Hashable, Sendable {
     /// - parameter customElementNames: A `Set` of HTML tag names which should be processed into custom elements.
     ///     Any HTML tags encountered that aren't specified in the set will be treated as if they were part of the normal markdown text.
     /// - parameter baseUrl: The document's base url. If `nil`, `url` will be used instead, with the last path component (the file itself) omitted
+    @_documentation(visibility: internal)
     @available(*, deprecated, renamed: "init(contentsOf:customElementNames:baseUrl:)")
     public init(processingContentsOf url: URL, customElementNames: Set<String> = [], baseUrl: URL? = nil) throws {
         try self.init(contentsOf: url, customElementNames: customElementNames, baseUrl: baseUrl)

Sources/SpeziFoundation/Misc/SequenceExtensions.swift
@@ -126,8 +126,6 @@ extension Collection {
 extension Array {
     /// Unsafely accesses the element at the specified index, without performing any bounds checking.
     ///
-    /// If `position` is not a valid index, the program will continue running as if 
-    ///
     /// - Warning: This function trades safety for performance.
     ///     Use ``subscript(unsafe:)`` only in situations where your program can guarantee that `position` is valid for indexing into the array,
     ///     and only if the default bounds checking performed by the runtime has proven to be a significant performance problem.
@@ -137,7 +135,7 @@ extension Array {
     public subscript(unsafe position: Int) -> Element {
         @_transparent
         get {
-            withUnsafeBufferPointer { $0[position] }
+            withUnsafeBufferPointer { $0.baseAddress.unsafelyUnwrapped.advanced(by: position).pointee }
         }
     }
 }

Sources/SpeziFoundation/Misc/Sorting.swift
@@ -1,0 +1,69 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+public import Foundation
+
+
+extension Sequence {
+    /// Returns the elements of the sequence, sorted using the given array of existential `SortComparator`s to compare elements.
+    ///
+    /// - Note: This function exists as an alternative to Foundation's `sorted(using:)` implementation, for the case where the comparators are of heterogeneous types.
+    ///
+    /// - parameter comparators: a sequence of comparators used to compare elements.
+    ///     The first comparator specifies the primary comparator to be used in
+    ///     sorting the sequence's elements. Any subsequent comparators are used
+    ///     to further refine the order of elements with equal values.
+    /// - returns: an array of the elements sorted using `comparators`.
+    @_disfavoredOverload
+    @inlinable
+    public func sorted(using comparators: some Sequence<any SortComparator<Element>>) -> [Element] {
+        var copy = Array(self)
+        copy.sort(using: comparators)
+        return copy
+    }
+}
+
+
+extension MutableCollection where Self: RandomAccessCollection {
+    /// Sorts the collection using the given array of existential `SortComparator`s to compare elements.
+    ///
+    /// - Note: This function exists as an alternative to Foundation's `sort(using:)` implementation, for the case where the comparators are of heterogeneous types.
+    ///
+    /// - parameter comparators: a sequence of comparators used to compare elements.
+    ///     The first comparator specifies the primary comparator to be used in
+    ///     sorting the sequence's elements. Any subsequent comparators are used
+    ///     to further refine the order of elements with equal values.
+    @_disfavoredOverload
+    @inlinable
+    public mutating func sort(using comparators: some Sequence<any SortComparator<Element>>) {
+        self.sort { lhs, rhs in
+            comparators.compare(lhs, rhs) == .orderedAscending
+        }
+    }
+}
+
+
+extension Sequence {
+    @inlinable
+    func compare<Compared>(
+        _ lhs: Compared,
+        _ rhs: Compared
+    ) -> ComparisonResult where Element == any SortComparator<Compared> {
+        for comparator in self {
+            let result = comparator.compare(lhs, rhs)
+            switch result {
+            case .orderedAscending, .orderedDescending:
+                return result
+            case .orderedSame:
+                continue
+            }
+        }
+        // all compared equal
+        return .orderedSame
+    }
+}

Sources/SpeziLocalization/LocalizedFileResources/LocalizationKey.swift
@@ -27,7 +27,8 @@ public import Foundation
 /// - ``description``
 ///
 /// ### Instance Methods
-/// - ``score(against:using:)``
+/// - ``score(against:using:)-(Locale,_)``
+/// - ``score(against:using:)-(Locale.Language,_)``
 public struct LocalizationKey: Sendable {
     /// The `en-US` localization key
     public static let enUS = Self(language: .init(identifier: "en"), region: .unitedStates)

Tests/SpeziFoundationTests/SequenceExtensionTests.swift
@@ -96,6 +96,29 @@ struct SequenceExtensions {
         #expect(input[unsafe: 1] == 1)
         #expect(input[unsafe: 2] == 2)
     }
+    
+    
+    @Test
+    func multiComparatorSorting() {
+        struct Cat: Equatable {
+            let name: String
+            let age: Int
+        }
+        var cats = [
+            Cat(name: "Terrace", age: 7),
+            Cat(name: "Pixel", age: 5),
+            Cat(name: "Pixel", age: 4)
+        ]
+        cats.sort(using: [
+            KeyPathComparator<Cat>(\.name),
+            KeyPathComparator<Cat>(\.age)
+        ] as [any SortComparator<Cat>])
+        #expect(cats == [
+            Cat(name: "Pixel", age: 4),
+            Cat(name: "Pixel", age: 5),
+            Cat(name: "Terrace", age: 7)
+        ])
+    }
 }
 
 

commit 0a44bfca9bce92dc9ff30994f658086b86fed631

ManagedTaskQueue fix (#52)
# ManagedTaskQueue fix

## :recycle: Current situation & Problem
the `ManagedTaskQueue` API introduced in #50 contained a bug where only
the initial `limit` tasks would actually run in parallel; anything
submitted after that would run serially.


## :gear: Release Notes
- fixed `ManagedTaskQueue` not running all tasks in parallel


## :books: Documentation
n/a


## :white_check_mark: Testing
n/a (maybe will be able to come up w/ smth here but no promises)


## :pencil: Code of Conduct & Contributing Guidelines
By creating and submitting this pull request, you agree to follow our
[Code of
Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
and [Contributing
Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
- [x] I agree to follow the [Code of
Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
and [Contributing
Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

Package.swift
@@ -89,6 +89,9 @@ let package = Package(
                 .target(name: "SpeziLocalization"),
                 .target(name: "SpeziFoundation")
             ],
+            resources: [
+                .process("Resources")
+            ],
             swiftSettings: [.enableUpcomingFeature("ExistentialAny")],
             plugins: [] + swiftLintPlugin()
         )

Sources/SpeziFoundation/Concurrency/ManagedTaskQueue.swift
@@ -15,7 +15,7 @@ public struct ManagedTaskQueue: ~Copyable {
     
     @usableFromInline let continuation: AsyncStream<Operation>.Continuation
     
-    fileprivate init(continuation: AsyncStream<Operation>.Continuation) {
+    fileprivate init(_ continuation: AsyncStream<Operation>.Continuation) {
         self.continuation = continuation
     }
     
@@ -65,7 +65,7 @@ public func withManagedTaskQueue(limit: Int, _ body: sending @escaping (_ taskQu
     await withTaskGroup(of: TaskType.self) { group in
         let body = (consume boxedBody)()
         group.addTask {
-            await body(ManagedTaskQueue(continuation: continuation))
+            await body(ManagedTaskQueue(continuation))
             continuation.finish()
             return .scheduler
         }
@@ -77,12 +77,12 @@ public func withManagedTaskQueue(limit: Int, _ body: sending @escaping (_ taskQu
                 // we're at the limit, and need to wait for a slot to become available.
                 // we can't simply call `group.next()` and ignore the result,
                 // since the first task will be the initial one that calls `body`.
-                while let taskType = await group.next() {
+                loop: while let taskType = await group.next() {
                     switch taskType {
                     case .scheduler:
                         continue
                     case .worker:
-                        break
+                        break loop
                     }
                 }
                 activeWorkers -= 1

Sources/SpeziLocalization/LocalizedFileResources/LocalizedFileResolution.swift
@@ -100,7 +100,7 @@ extension LocalizedFileResolution {
             }
             if let equallyBestRanked = candidates.lazy.chunked(by: { $0.score == $1.score }).first, !equallyBestRanked.isEmpty { // always true
                 guard equallyBestRanked.count == 1 else {
-                    var errorMsg = "Error: Found multiple candidates for \(resource.name) @ \(language.minimalIdentifier), all of which are equally ranked!"
+                    var errorMsg = "Found multiple candidates for \(resource.name) @ \(language.minimalIdentifier), all of which are equally ranked!"
                     for candidate in candidates {
                         errorMsg.append("\n- \(candidate.score) @ \(candidate.fileResource.fullFilenameIncludingLocalization)")
                     }

Tests/SpeziFoundationTests/ManagedTaskQueueTests.swift
@@ -17,34 +17,58 @@ import Testing
 struct ManagedTaskQueueTests {
     @MainActor
     private final class OperationsTracker: Sendable {
+        struct Operation: Hashable {
+            let id: Int
+            let startDate: Date
+            let endDate: Date
+            
+            var timeRange: Range<Date> {
+                startDate..<endDate
+            }
+            
+            static func == (lhs: Self, rhs: Self) -> Bool {
+                lhs.id == rhs.id
+            }
+            func hash(into hasher: inout Hasher) {
+                hasher.combine(id)
+            }
+        }
+        
         let expectedLimit: Int
-        private(set) var active = Set<Int>()
-        private(set) var completed = Set<Int>()
+        private(set) var active = Set<Operation>()
+        private(set) var completed = Set<Operation>()
         private(set) var maxObservedConcurrency = 0
+        
+        var activeIds: Set<Int> {
+            active.mapIntoSet(\.id)
+        }
+        var completedIds: Set<Int> {
+            completed.mapIntoSet(\.id)
+        }
         
         nonisolated init(expectedLimit: Int) {
             self.expectedLimit = expectedLimit
         }
         
-        func trackBegin(of operation: Int) throws {
+        func trackBegin(of operationId: Int) throws {
             #expect(
                 active.count < expectedLimit,
-                "adding \(operation); active: \(active.sorted()); completed: \(completed.sorted())"
+                "adding \(operationId); active: \(activeIds.sorted()); completed: \(completedIds.sorted())"
             )
-            try #require(!completed.contains(operation), "Operation '\(operation)' has already run")
-            try #require(active.insert(operation).inserted, "Operation '\(operation)' is already running")
+            try #require(!completed.contains { $0.id == operationId }, "Operation '\(operationId)' has already run")
+            try #require(active.insert(.init(id: operationId, startDate: .now, endDate: .distantFuture)).inserted, "Operation '\(operationId)' is already running")
             maxObservedConcurrency = max(maxObservedConcurrency, active.count)
         }
         
-        func trackEnd(of operation: Int) throws {
+        func trackEnd(of operationId: Int) throws {
             #expect(
                 active.count <= expectedLimit,
-                "removing \(operation); active: \(active.sorted()); completed: \(completed.sorted())"
+                "removing \(operationId); active: \(activeIds.sorted()); completed: \(completedIds.sorted())"
             )
-            try #require(!completed.contains(operation), "Operation '\(operation)' has already run")
-            let idx = try #require(active.firstIndex(of: operation), "Operation '\(operation)' is not running")
-            active.remove(at: idx)
-            completed.insert(operation)
+            try #require(!completed.contains { $0.id == operationId }, "Operation '\(operationId)' has already run")
+            let idx = try #require(active.firstIndex { $0.id == operationId }, "Operation '\(operationId)' is not running")
+            let operation = active.remove(at: idx)
+            completed.insert(.init(id: operation.id, startDate: operation.startDate, endDate: .now))
         }
     }
     
@@ -63,7 +87,36 @@ struct ManagedTaskQueueTests {
             }
         }
         #expect(await tracker.active.isEmpty)
-        #expect(await tracker.completed == Set(0..<numTasks))
+        #expect(await tracker.completedIds == Set(0..<numTasks))
         #expect(await tracker.maxObservedConcurrency <= concurrencyLimit)
+    }
+    
+    
+    @Test(arguments: Array(1...20))
+    func ordering(limit: Int) async throws {
+        let start = Date.now
+        let tracker = OperationsTracker(expectedLimit: limit)
+        await withManagedTaskQueue(limit: limit) { taskQueue in
+            for idx in 0..<(limit * 3) {
+                taskQueue.addTask {
+                    try! await tracker.trackBegin(of: idx)
+                    try! await Task.sleep(for: .seconds(2))
+                    try! await tracker.trackEnd(of: idx)
+                }
+            }
+        }
+        let end = Date.now
+        let operations = await tracker.completed.sorted(using: [
+            KeyPathComparator(\.startDate),
+            KeyPathComparator(\.id)
+        ])
+        for timestamp in stride(from: start.addingTimeInterval(0.25), through: end.addingTimeInterval(-2.5), by: 0.5) {
+            let numActiveTasks = operations.count { $0.timeRange.contains(timestamp) }
+            let expectedRange = (limit - 1)...limit
+            #expect(
+                expectedRange.contains(numActiveTasks),
+                "[\(timestamp.timeIntervalSince(start))]Expected \(expectedRange) active tasks; got \(numActiveTasks) (all task logging: \(operations))"
+            )
+        }
     }
 }