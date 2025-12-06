427c2885f575a4317cd9ae5581517ebedf375209
Philipp Nagy <philipp.nagy@tum.de>
2025-12-06 14:53:28 +0100

    Fix SwiftLint

--- Sources/SpeziFoundation/Misc/SpeziLog.swift
+++ Sources/SpeziFoundation/Misc/SpeziLog.swift
@@ -8,22 +8,19 @@
 
 
 #if canImport(os)
-@_documentation(visibility: internal)
-@_exported import os
+@_documentation(visibility: internal) @_exported import OSLog
 
 /// A unified type alias for the system `Logger` on supported Apple platforms.
 ///
 /// > Important: On non-Apple platforms (such as Linux or Windows) where `os` is unavailable, this type automatically aliases to `Logging.Logger` from [swift-log](https://github.com/apple/swift-log). To ensure source compatibility, an extension is provided for `Logging.Logger` that adds the `init(subsystem:category:)` initializer.
-@available(iOS 14.0, *)
 public typealias SpeziLogger = Logger
 #else
-@_documentation(visibility: internal)
-@_exported import Logging
+@_documentation(visibility: internal) @_exported import Logging
 
 /// A unified type alias for `Logging.Logger` on non-Apple platforms.
 public typealias SpeziLogger = Logger
 
-public extension Logger {
+extension Logger {
     /// Creates a logger instance using the `os.Logger` initialization style.
     ///
     /// - Parameters:
