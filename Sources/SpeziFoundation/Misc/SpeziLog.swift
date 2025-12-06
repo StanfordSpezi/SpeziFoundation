149e1676be2f9a7bb09280da9e89456996cf1284
Philipp Nagy <philipp.nagy@tum.de>
2025-12-06 13:39:07 +0100

    Add SpeziLogger typealias

---
+++ Sources/SpeziFoundation/Misc/SpeziLog.swift
@@ -0,0 +1,37 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+
+#if canImport(os)
+@_documentation(visibility: internal)
+@_exported import os
+
+/// A unified type alias for the system `Logger` on supported Apple platforms.
+///
+/// > Important: On non-Apple platforms (such as Linux or Windows) where `os` is unavailable, this type automatically aliases to `Logging.Logger` from [swift-log](https://github.com/apple/swift-log). To ensure source compatibility, an extension is provided for `Logging.Logger` that adds the `init(subsystem:category:)` initializer.
+@available(iOS 14.0, *)
+public typealias SpeziLogger = Logger
+#else
+@_documentation(visibility: internal)
+@_exported import Logging
+
+/// A unified type alias for `Logging.Logger` on non-Apple platforms.
+public typealias SpeziLogger = Logger
+
+public extension Logger {
+    /// Creates a logger instance using the `os.Logger` initialization style.
+    ///
+    /// - Parameters:
+    ///   - subsystem: Mapped to `label`.
+    ///   - category: Stored in metadata key "category".
+    public init(subsystem: String, category: String) {
+        self.init(label: subsystem)
+        self[metadataKey: "category"] = "\(category)"
+    }
+}
+#endif
