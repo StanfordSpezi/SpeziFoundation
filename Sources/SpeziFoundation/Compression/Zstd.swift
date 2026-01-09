aefc71dc81a2657c9884750b8608c970a1723d85
Lukas Kollmer <hey@lukaskollmer.de>
2026-01-09 21:17:05 +0100

    add Zstd compression algorithm (#49)
    # add Zstd compression algorithm
    
    ## :recycle: Current situation & Problem
    adds the [Zstd](https://en.wikipedia.org/wiki/Zstd) compression
    algorithm.
    
    
    ## :gear: Release Notes
    - added support for the [Zstd](https://en.wikipedia.org/wiki/Zstd)
    compression algorithm
    
    
    ## :books: Documentation
    yes
    
    
    ## :white_check_mark: Testing
    yes
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

---
+++ Sources/SpeziFoundation/Compression/Zstd.swift
@@ -0,0 +1,174 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+// swiftlint:disable attributes
+
+public import Foundation
+public import libzstd
+import ThreadLocal
+
+/// A wrapper around the [`Zstd`](https://github.com/facebook/zstd) compression library.
+///
+/// ## Topics
+///
+/// ### Operations
+/// - ``compress(_:)``
+/// - ``compress(_:options:)``
+/// - ``decompress(_:)``
+///
+/// ### Supporting Types
+/// - ``CompressionOptions``
+/// - ``CompressionError``
+/// - ``DecompressionError``
+public enum Zstd: CompressionAlgorithm {
+    public typealias DecompressionError = CompressionError
+    
+    public enum CompressionError: Error {
+        /// The input sequence didn't have an underlying contiguous storage
+        case invalidInput
+        case notEnoughMemory
+        case other(ZSTD_ErrorCode)
+    }
+    
+    public struct CompressionOptions: CompressionOptionsProtocol {
+        /// The compression level.
+        ///
+        /// ## Topics
+        ///
+        /// ### Predefined Compression Levels
+        /// - ``default``
+        /// - ``minRegular``
+        /// - ``maxRegular``
+        ///
+        /// ### Initializers
+        /// - ``init(rawValue:)``
+        ///
+        /// ### Instance Properties
+        /// - ``rawValue``
+        public struct Level: RawRepresentable, Sendable {
+            /// The minimum regular compression level.
+            ///
+            /// This level maximises speed, at the cost of compression.
+            @inlinable public static var minRegular: Self {
+                Self(rawValue: 0)
+            }
+            /// The maximum regular compression level.
+            ///
+            /// This level maximises compression, at the cost of speed.
+            @inlinable public static var maxRegular: Self {
+                Self(rawValue: 20)
+            }
+            
+            /// The default compression level
+            @inlinable public static var `default`: Self {
+                Self(rawValue: ZSTD_CLEVEL_DEFAULT)
+            }
+            
+            /// The compression level's underlying raw value.
+            public let rawValue: Int32
+            
+            /// Creates a compression level from a raw value.
+            ///
+            /// - Note: This initializer performs no validation of the value. The caller must ensure that it is valid.
+            @inlinable
+            public init(rawValue: Int32) {
+                self.rawValue = rawValue
+            }
+        }
+        
+        /// The compression level.
+        public let level: Level
+        
+        @inlinable
+        public init() {
+            self.init(level: .default)
+        }
+        
+        /// Creates compression options.
+        @inlinable
+        public init(level: Level) {
+            self.level = level
+        }
+    }
+    
+    
+    /// The compression context
+    @ThreadLocal(deallocator: .custom(ZSTD_freeCCtx))
+    private static var cCtx: OpaquePointer = ZSTD_createCCtx()
+    
+    /// The decompression context
+    @ThreadLocal(deallocator: .custom(ZSTD_freeDCtx))
+    private static var dCtx: OpaquePointer = ZSTD_createDCtx()
+    
+    
+    public static func compress(_ bytes: borrowing some Collection<UInt8>, options: CompressionOptions) throws(CompressionError) -> Data {
+        let inputLen = bytes.count
+        let result: Result<Data, CompressionError>? = bytes.withContiguousStorageIfAvailable { inputBuffer in
+            assert(inputBuffer.count == inputLen)
+            guard let inputBufferPtr = inputBuffer.baseAddress else {
+                return .failure(.invalidInput)
+            }
+            let outputBufferSize = ZSTD_compressBound(inputBuffer.count)
+            let outputBuffer: UnsafeMutablePointer<UInt8> = .allocate(capacity: outputBufferSize)
+            let result = ZSTD_compressCCtx(Self.cCtx, outputBuffer, outputBufferSize, inputBufferPtr, inputLen, options.level.rawValue)
+            if ZSTD_isError(result) == 0 {
+                return .success(Data(bytesNoCopy: outputBuffer, count: result, deallocator: .free))
+            } else {
+                switch ZSTD_getErrorCode(result) {
+                case ZSTD_error_memory_allocation:
+                    return .failure(.notEnoughMemory)
+                case let errorCode:
+                    return .failure(.other(errorCode))
+                }
+            }
+        }
+        guard let result else {
+            // the input didn't have a contiguous storage representation
+            throw .invalidInput
+        }
+        return try result.get()
+    }
+    
+    
+    public static func decompress(_ bytes: borrowing some Collection<UInt8>) throws(DecompressionError) -> Data {
+        let inputLen = bytes.count
+        let result: Result<Data, DecompressionError>? = bytes.withContiguousStorageIfAvailable { inputBuffer in
+            guard let inputBufferPtr = inputBuffer.baseAddress else {
+                return .failure(.invalidInput)
+            }
+            assert(inputBuffer.count == inputLen)
+            switch ZSTD_getFrameContentSize(inputBufferPtr, inputLen) {
+            case ZSTD_CONTENTSIZE_ERROR:
+                // likely not compressed by Zstd
+                return .failure(.invalidInput)
+            case ZSTD_CONTENTSIZE_UNKNOWN:
+                // could try to use streaming decompression for this at some point in the future
+                return .failure(.invalidInput)
+            case let contentSize:
+                let contentSize = Int(contentSize)
+                let outputBuffer: UnsafeMutablePointer<UInt8> = .allocate(capacity: contentSize)
+                let result = ZSTD_decompressDCtx(Self.dCtx, outputBuffer, contentSize, inputBufferPtr, inputBuffer.count)
+                if ZSTD_isError(result) == 0 {
+                    return .success(Data(bytesNoCopy: outputBuffer, count: contentSize, deallocator: .free))
+                } else {
+                    switch ZSTD_getErrorCode(result) {
+                    case ZSTD_error_memory_allocation:
+                        return .failure(.notEnoughMemory)
+                    case let errorCode:
+                        return .failure(.other(errorCode))
+                    }
+                }
+            }
+        }
+        guard let result else {
+            // the input didn't have a contiguous storage representation
+            throw .invalidInput
+        }
+        return try result.get()
+    }
+}
--- Tests/SpeziFoundationTests/CompressionAlgorithmTests.swift
+++ Tests/SpeziFoundationTests/CompressionAlgorithmTests.swift
@@ -13,11 +13,21 @@ import Testing
 
 @Suite
 struct CompressionAlgorithmTests {
+    private let longInput: Data
+    
+    init() throws {
+        var longText = ""
+        for _ in 0..<40_000 {
+            longText.append(try #require(Self.textSnippets.randomElement()))
+        }
+        self.longInput = try #require(longText.data(using: .utf8))
+    }
+    
     @Test
     func zlib() throws {
         let input = try #require(String(repeating: "Hello Spezi :)", count: 1000).data(using: .utf8))
-        let compressed = try input.compressed(using: Zlib.self)
         #expect(input.count == 14_000)
+        let compressed = try input.compressed(using: Zlib.self, options: .init(level: .bestCompression))
         #expect(compressed.count == 70)
         #expect(compressed == Data([
             120, 218, 237, 199, 49, 13, 0, 32, 12, 0, 48, 43, 188, 88, 64, 193, 126, 52, 236,
@@ -28,4 +38,75 @@ struct CompressionAlgorithmTests {
         let decompressed = try compressed.decompressed(using: Zlib.self)
         #expect(decompressed == input)
     }
+    
+    @Test
+    func zlibHmmm() throws {
+        let input = longInput
+        let compressed1 = try input.compressed(using: Zlib.self, options: .init(level: .bestSpeed))
+        let compressed2 = try input.compressed(using: Zlib.self, options: .init(level: .bestCompression))
+        print(input.count, compressed1.count, compressed2.count)
+        #expect(try compressed1.decompressed(using: Zlib.self).elementsEqual(input))
+        #expect(try compressed2.decompressed(using: Zlib.self).elementsEqual(input))
+    }
+    
+    @Test
+    func zlibCompressionOptions() throws {
+        let input = longInput
+        let compressed1 = try input.compressed(using: Zlib.self, options: .init(level: .bestSpeed))
+        let compressed2 = try input.compressed(using: Zlib.self, options: .init(level: .bestCompression))
+        #expect(compressed1.count > compressed2.count)
+        print(input.count, compressed1.count, compressed2.count)
+        #expect(try compressed1.decompressed(using: Zlib.self).elementsEqual(input))
+        #expect(try compressed2.decompressed(using: Zlib.self).elementsEqual(input))
+    }
+    
+    
+    @Test
+    func zstd() throws {
+        let input = try #require(String(repeating: "Hello Spezi :)", count: 1000).data(using: .utf8))
+        #expect(input.count == 14_000)
+        let compressed = try input.compressed(using: Zstd.self)
+        print(Array(compressed))
+        #expect(compressed.count == 32)
+        #expect(compressed == Data([
+            40, 181, 47, 253, 96, 176, 53, 181, 0, 0, 112, 72, 101, 108, 108, 111,
+            32, 83, 112, 101, 122, 105, 32, 58, 41, 1, 0, 159, 54, 248, 169, 4
+        ]))
+        let decompressed = try compressed.decompressed(using: Zstd.self)
+        #expect((decompressed == input) as Bool)
+    }
+    
+    @Test
+    func zstdCompressionOptions() throws {
+        let input = longInput
+        let compressed1 = try input.compressed(using: Zstd.self, options: .init(level: .minRegular))
+        let compressed2 = try input.compressed(using: Zstd.self, options: .init(level: .maxRegular))
+        #expect(compressed1.count > compressed2.count)
+        print(input.count, compressed1.count, compressed2.count)
+        #expect(try compressed1.decompressed(using: Zstd.self).elementsEqual(input))
+        #expect(try compressed2.decompressed(using: Zstd.self).elementsEqual(input))
+    }
+}
+
+
+// swiftlint:disable line_length
+
+extension CompressionAlgorithmTests {
+    private static let textSnippets: [String] = [
+        """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """,
+        """
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        """,
+        """
+        But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?
+        """,
+        """
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+        """,
+        """
+        On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
+        """
+    ]
 }
