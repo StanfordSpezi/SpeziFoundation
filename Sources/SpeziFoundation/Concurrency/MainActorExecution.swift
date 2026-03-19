f1ed9caeae217cb63872a504a2c6ef8d9cb4e09f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-03-18 19:15:53 -0700

    Remove Breaking Changes

--- Sources/SpeziFoundation/Concurrency/MainActorExecution.swift
+++ Sources/SpeziFoundation/Concurrency/MainActorExecution.swift
@@ -20,7 +20,7 @@ import Foundation
     *, deprecated, message: "Do not use this method as it breaks structured concurrency. Consider using other mechanisms like an AsyncStream."
 )
 public func runOrScheduleOnMainActor(
-    performThis block: @MainActor @escaping () -> Void
+    _ block: @MainActor @escaping () -> Void
 ) {
     if Thread.isMainThread {
         MainActor.assumeIsolated {
